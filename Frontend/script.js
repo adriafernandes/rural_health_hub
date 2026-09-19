d26fdf1d3a526176a2a3f97579a1961b9035b949
adriafernandes <adriafernandes2008@gmail.com>
2026-09-19 12:44:09 +0530

    Complete healthcare consultation flow

--- Frontend/script.js
+++ Frontend/script.js
@@ -1,3 +1,18 @@
-document.getElementById("findButton").addEventListener("click", function () {
-    window.location.href = "facilities.html";
-});
+// Find Healthcare button
+const findButton = document.getElementById("findButton");
+
+if (findButton) {
+    findButton.addEventListener("click", function () {
+        window.location.href = "facilities.html";
+    });
+}
+
+
+// Request Consultation button
+const consultationButton = document.getElementById("consultationButton");
+
+if (consultationButton) {
+    consultationButton.addEventListener("click", function () {
+        window.location.href = "appointment.html";
+    });
+}
