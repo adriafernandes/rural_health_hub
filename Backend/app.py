d836bd6198fb2acb097504cd2a2e8efc05b14e73
adriafernandes <adriafernandes2008@gmail.com>
2026-09-19 16:14:35 +0530

    Connect facilities to MySQL backend

--- Backend/app.py
+++ Backend/app.py
@@ -1,10 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
+from database import get_db_connection
+
 app = Flask(__name__)
+
 
 @app.route("/")
 def home():
-    return "Rural Health Hub is running!"
+    return "Rural Health Hub Backend is running!"
+
+
+@app.route("/api/facilities")
+def get_facilities():
+    connection = get_db_connection()
+    cursor = connection.cursor(dictionary=True)
+
+    cursor.execute("SELECT * FROM facilities")
+    facilities = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return jsonify(facilities)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
