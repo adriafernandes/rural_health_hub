ba79ad1da205316dd2b86aa6f94f1cf4c16fcd5c
adriafernandes <adriafernandes2008@gmail.com>
2026-09-19 23:35:37 +0530

    Update database connection for deployment

--- Backend/database.py
+++ Backend/database.py
@@ -1,12 +1,14 @@
+import os
 import mysql.connector
 
 
 def get_db_connection():
     connection = mysql.connector.connect(
-        host="localhost",
-        user="root",
-        password="",
-        database="rural_health"
+        host=os.getenv("MYSQLHOST", "localhost"),
+        port=int(os.getenv("MYSQLPORT", 3306)),
+        user=os.getenv("MYSQLUSER", "root"),
+        password=os.getenv("MYSQLPASSWORD", ""),
+        database=os.getenv("MYSQLDATABASE", "rural_health")
     )
 
     return connection
